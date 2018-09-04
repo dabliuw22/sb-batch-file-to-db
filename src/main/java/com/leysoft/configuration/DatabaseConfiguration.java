2dc44e1cc0844836a053724277dad5dbf888d391
Will Leyton <will.leyton@rappi.com>
2018-09-04 11:38:53 -0500

    Added NamedParameterJdbcTemplate in DatabaseConfiguration

--- src/main/java/com/leysoft/configuration/DatabaseConfiguration.java
+++ src/main/java/com/leysoft/configuration/DatabaseConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -39,5 +40,10 @@ public class DatabaseConfiguration {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+    
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+    	return new NamedParameterJdbcTemplate(dataSource);
     }
 }
